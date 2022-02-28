42931e3d956b61fc333f1226d9ee51d98944135f
jinn2u <bsybear623@gmail.com>
2022-02-28 17:13:54 +0900

    feat: enter키 입력시 input update하기

---
+++ src/components/AutoComplete/utils/useUpdateAutoComplete.ts
@@ -0,0 +1,20 @@
+import { Dispatch, SetStateAction } from 'react';
+import { TypeDropdownList, TypeWordList } from '../types';
+import { createDropdownList } from './createDropdownList';
+
+export const useUpdateAutoComplete = (
+  word: string,
+  wordList: TypeWordList,
+  setShowDropdown: Dispatch<SetStateAction<boolean>>,
+  setAutoCompleteInput: Dispatch<SetStateAction<string>>,
+  setDropdownList: Dispatch<SetStateAction<TypeDropdownList>>,
+  handleSubmit: () => void,
+) => {
+  setShowDropdown(false);
+  setAutoCompleteInput(word);
+
+  const newDropdownList = createDropdownList(wordList, word, 7);
+
+  setDropdownList(newDropdownList);
+  handleSubmit();
+};
