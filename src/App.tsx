66b746b431cdf41bf97f0ddc4f7fded9e3d54bdd
jinn2u <bsybear623@gmail.com>
2022-02-28 16:48:09 +0900

    feat: App에 dropdown 함수 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import AutoComplete from './components/AutoComplete';
+import { MOCK_DATA } from './components/AutoComplete/MOCK_DATA';
 
 function App() {
   const [autoCompleteInput, setAutoCompleteInput] = useState('');
-
+  const [wordList, setWordList] = useState(MOCK_DATA);
   return (
     <AutoComplete
       autoCompleteInput={autoCompleteInput}
       setAutoCompleteInput={setAutoCompleteInput}
+      wordList={wordList}
     />
   );
 }
--- src/components/AutoComplete/index.tsx
+++ src/components/AutoComplete/index.tsx
@@ -1,17 +1,32 @@
-import { ChangeEvent, useCallback } from 'react';
+import { ChangeEvent, useCallback, useState } from 'react';
 import { Input } from './style';
-import { Props } from './types';
+import { Props, TypeDropdownList } from './types';
+import { createDropdownListAndSetDropDownOpen } from './utils/createDropdownListAndSetDropDownOpen';
 
 const AutoComplete = ({
   width = 300,
   setAutoCompleteInput,
   autoCompleteInput,
+  wordList,
   ...props
 }: Props) => {
-  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setAutoCompleteInput(value);
-  }, []);
+  const [dropdownList, setDropdownList] = useState<TypeDropdownList>([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      setAutoCompleteInput(value);
+
+      if (!value.length) {
+        setShowDropdown(false);
+        return setDropdownList([]);
+      }
+      createDropdownListAndSetDropDownOpen(wordList, value, setDropdownList, setShowDropdown);
+    },
+    [wordList],
+  );
+
   return <Input width={width} onChange={handleChange} value={autoCompleteInput} />;
 };
 export default AutoComplete;
