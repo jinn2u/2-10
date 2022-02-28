a83acc0388d2934a036e3866ea96638fe1a5f298
jinn2u <bsybear623@gmail.com>
2022-02-28 16:47:36 +0900

    feat: dropdown과 wordList의 타입

--- src/components/AutoComplete/types.ts
+++ src/components/AutoComplete/types.ts
@@ -4,4 +4,7 @@ export interface Props {
   width?: number;
   setAutoCompleteInput: Dispatch<SetStateAction<string>>;
   autoCompleteInput: string;
+  wordList: TypeWordList;
 }
+export type TypeWordList = { id: number; name: string }[];
+export type TypeDropdownList = { id: number; name: string; isSelected: boolean }[];
