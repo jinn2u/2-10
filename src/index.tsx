776f1506f310b7349a67134e69c43c69a2ff4f46
jinn2u <bsybear623@gmail.com>
2022-03-02 03:43:05 +0900

    feat: globalStyles 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './theme/GlobalStyles';
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyles />
     <App />
   </Provider>,
   document.getElementById('root'),
